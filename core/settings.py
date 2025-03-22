f24acfad60c8dce411fcf9491e4192e2b4d879de
Aakash Singh <mail@singhaakash.dev>
2025-03-22 23:27:40 +0530

    use default logging

--- core/settings.py
+++ core/settings.py
@@ -135,28 +135,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-        },
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": "WARNING",
-    },
-    "loggers": {
-        "django": {
-            "handlers": ["console"],
-            "level": env("DJANGO_LOG_LEVEL", default="INFO"),
-            "propagate": False,
-        },
-    },
-}
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
